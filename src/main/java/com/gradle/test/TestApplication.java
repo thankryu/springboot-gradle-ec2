537c299b401a14e6b5560b8ceaf56247d26a79ff
Doil <thankryu@gmail.com>
2022-09-29 22:53:38 +0900

    #1 gradle 설정 변경

--- src/main/java/com/gradle/test/TestApplication.java
+++ src/main/java/com/gradle/test/TestApplication.java
@@ -2,12 +2,22 @@ package com.gradle.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
+	}
+
+	@RequestMapping
+	public String Hello(){
+		System.out.println("hi kyounghan!");
+		return "hello!!!";
 	}
 
 }
