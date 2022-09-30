8b122bfd297a2750472c3279577c1de5489c0a7a
유경한 <thankryu@gmail.com>
2022-09-30 18:01:11 +0900

    junit 테스트 적용

---
+++ src/test/java/com/gradle/springboot/main/MainControllerTest.java
@@ -0,0 +1,31 @@
+package com.gradle.springboot.main;
+
+
+import com.gradle.springboot.main.controller.MainController;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = MainController.class)
+public class MainControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void Hello222() throws Exception{
+        String hello = "hello";
+
+        mvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(hello));
+    }
+}
