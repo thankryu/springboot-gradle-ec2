d7abbbb7cc3d877885e1e481673ff7a546ae79f5
유경한 <thankryu@gmail.com>
2022-09-30 17:59:22 +0900

    최초 구동 class 이름 변경

--- src/main/java/com/gradle/springboot/Application.java
+++ src/main/java/com/gradle/springboot/Application.java
@@ -1,4 +1,4 @@
-package com.gradle.test;
+package com.gradle.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@SpringBootApplication
 @RestController
-public class TestApplication {
+@SpringBootApplication
+public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+	 public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
 	}
 
 	@RequestMapping
