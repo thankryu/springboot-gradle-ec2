10c1c6a83b54ba9d8d85e6a076d13f689948f0c0
유경한 <thankryu@gmail.com>
2022-10-04 18:33:40 +0900

    #3 이동 실행 url

---
+++ src/main/java/com/gradle/springboot/image/ImageController.java
@@ -0,0 +1,21 @@
+package com.gradle.springboot.image;
+
+import com.gradle.springboot.util.Move;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+
+@RestController
+public class ImageController {
+
+    File folder1 = new File("C:\\test1");
+    File folder2 = new File("C:\\test2");
+
+    @GetMapping("/move")
+    public void moveFile(){
+        System.out.println("폴더이동");
+        Move.copy(folder1, folder2);
+        Move.delete(folder1.toString());
+    }
+}
