d87fa269bd819768ad463f5b986fd2639ab98054
유경한 <thankryu@gmail.com>
2022-10-06 18:26:58 +0900

    #4 외부 로컬 스토리지 읽기 설정 추가

---
+++ src/main/java/com/gradle/springboot/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.gradle.springboot.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Value("${file.path}")
+    private String filePath;
+
+    /**
+     * <img alt="로컬 이미지 읽기" src="/file/test1.jpg">*
+     * @param registry
+     */
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations(filePath);
+    }
+
+}
