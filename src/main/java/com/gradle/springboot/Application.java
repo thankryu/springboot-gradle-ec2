d2711cd21b7c8ee8e0f5741f12dea2261139586c
유경한 <thankryu@gmail.com>
2022-10-11 16:01:54 +0900

    thymeleaf 적용 및 테스트

--- src/main/java/com/gradle/springboot/Application.java
+++ src/main/java/com/gradle/springboot/Application.java
@@ -2,11 +2,14 @@ package com.gradle.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
-@RestController
+@Controller
 @SpringBootApplication
 public class Application {
 
@@ -14,9 +17,16 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	@RequestMapping
-	public String Hello(){
+//	@RequestMapping
+//	public String Hello(){
+//		System.out.println("hi kyounghan!");
+//		return "2222hello!!!";
+//	}
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String Hello(Model model){
 		System.out.println("hi kyounghan!");
-		return "2222hello!!!";
+		model.addAttribute("hello", "서버에서 보내준 값 입니다.");
+		return "/hello";
 	}
 }
