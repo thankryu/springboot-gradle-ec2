57f6b1b2592155a1e136df92a2667ff0d2ad6f70
Doil <thankryu@gmail.com>
2022-11-11 18:00:26 +0900

    #10 화면 목록 조회 컨트롤러 처리

--- src/main/java/com/gradle/springboot/jpaTest/GuestBookController.java
+++ src/main/java/com/gradle/springboot/jpaTest/GuestBookController.java
@@ -1,16 +1,32 @@
 package com.gradle.springboot.jpaTest;
 
+import com.gradle.springboot.jpaTest.dto.PageRequestDTO;
+import com.gradle.springboot.jpaTest.service.GuestbookService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequiredArgsConstructor // 자동주입
 @RequestMapping("/guestbook")
 public class GuestBookController {
 
-    @GetMapping({"/","/list"})
-    public String list(){
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/")
+    public String index(){
+        return "redirect:/guestbook/list";
+    }
+
+    @GetMapping("/list")
+    public void list(@ModelAttribute PageRequestDTO pageRequestDTO , Model model){
+
+        // 실제로 model에 추가되는 데이터 : PageResultDTO
+        // => Model을 이용해 GuestbookServiecImpl에서 반환하는 PageResultDTO를 result 라는 이름으로 전달
+        model.addAttribute("result", service.getList(pageRequestDTO));
     }
 
 }
