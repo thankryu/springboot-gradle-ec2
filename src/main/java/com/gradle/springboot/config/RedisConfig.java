7d99e71bd0f0b69e780811975ed8d360ceb6f197
Kyounghan Ryu <thankryu@gmail.com>
2023-01-04 20:32:12 +0900

    #7 레디스 캐시 매니저 설정 추가

--- src/main/java/com/gradle/springboot/config/RedisConfig.java
+++ src/main/java/com/gradle/springboot/config/RedisConfig.java
@@ -1,15 +1,21 @@
 package com.gradle.springboot.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 @Configuration
 @EnableCaching
@@ -45,5 +51,17 @@ public class RedisConfig {
         // redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
         redisTemplate.setValueSerializer(genericJackson2JsonRedisSerializer);
         return redisTemplate;
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public CacheManager cacheManager() {
+        RedisCacheManager.RedisCacheManagerBuilder builder = RedisCacheManager.RedisCacheManagerBuilder.fromConnectionFactory(redisConnectionFactory());
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer())) // Value Serializer 변경
+                .prefixKeysWith("Test:") // Key Prefix로 "Test:"를 앞에 붙여 저장
+                .entryTtl(Duration.ofMinutes(30)); // 캐시 수명 30분
+        builder.cacheDefaults(configuration);
+        return builder.build();
     }
 }
